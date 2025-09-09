eebf8dc4ba9f9913e231de31c5f5a3913ff397eb
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2025-09-09 10:38:27 -0500

    initial commit

---
+++ Project 1/Fall2025-Project1-krhanson3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project1_krhanson3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
