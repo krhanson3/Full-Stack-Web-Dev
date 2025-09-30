3a9a6d8ae57887d32ca22cf34253b54098bdf475
Kaitlyn Hanson <krhanson3@crimson.ua.edu>
2025-09-30 09:46:50 -0500

    new project

---
+++ Fall2025-Project2-krhanson3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2025_Project2_krhanson3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
